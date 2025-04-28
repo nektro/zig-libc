cc258531ec8d668314ba5e42dfab373936966968
Meghan Denny <hello@nektro.net>
2025-04-28 12:54:42 -0700

    bring the switch back here zls supports it, impl decl still removes usingnamespace

--- mod.zig
+++ mod.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub const impl = @field(@This(), @tagName(builtin.target.os.tag));
+pub const impl = switch (builtin.target.os.tag) {
+    .linux => linux,
+    else => |v| @compileError("TODO: " ++ @tagName(v)),
+};
 pub const linux = @import("./linux.zig");
